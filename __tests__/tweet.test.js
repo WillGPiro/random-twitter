c857a0de1868a0d988b1e27f78d7e4f01bea381b
Will G Piro <willgpiro@gmail.com>
2020-03-24 13:22:19 -0700

    get tweet by id test and route complete

--- __tests__/tweet.test.js
+++ __tests__/tweet.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Tweet = require('../lib/Models/Tweets');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -18,9 +19,41 @@ describe('app routes', () => {
     return mongoose.connection.close();
   });
 
-  it('creates a tweet', () => {
-    return request(app);
+  it('creates a new tweet', () => {
+    return request(app)
+      .post('/api/v1/tweets')
+      .send({
+        handle: 'will@will.com',
+        text: 'Some random tweet' 
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          handle: 'will@will.com',
+          text: 'Some random tweet',
+          __v: 0
+        });
+      });
   });
 
-
+  it('gets a tweet by ID', () => {
+    return Tweet.create({
+      handle: 'will@will.com',
+      text: 'Some random tweet',
+    })
+      .then(tweet => {
+        return request(app)
+          .get(`/api/v1/tweets/${tweet.id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          handle: 'will@will.com',
+          text: 'Some random tweet',
+          __v:0
+        });
+      });
+  });
 });
+
+
